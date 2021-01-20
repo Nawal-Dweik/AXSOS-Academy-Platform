cc2a122bdfd3f6d66d25a3d589cda6d490b67243
nd2 <nawal.dweik@axsos.me>
2021-01-20 12:49:21 +0200

    Repositories & Services for algorithm & category & group activity

--- src/main/java/com/axsosacademy/axsosplatform/repositories/GroupActivityRepository.java
+++ src/main/java/com/axsosacademy/axsosplatform/repositories/GroupActivityRepository.java
@@ -1,7 +1,11 @@
 package com.axsosacademy.axsosplatform.repositories;
 
+import com.axsosacademy.axsosplatform.models.Algorithm;
 import com.axsosacademy.axsosplatform.models.GroupActivity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface GroupActivityRepository extends CrudRepository<GroupActivity, Long> {
+    public List<GroupActivity> findAll();
 }
--- src/main/java/com/axsosacademy/axsosplatform/services/GroupActivityService.java
+++ src/main/java/com/axsosacademy/axsosplatform/services/GroupActivityService.java
@@ -1,4 +1,12 @@
 package com.axsosacademy.axsosplatform.services;
 
+import com.axsosacademy.axsosplatform.repositories.CategoryRepository;
+import com.axsosacademy.axsosplatform.repositories.GroupActivityRepository;
+
 public class GroupActivityService {
+    private final GroupActivityRepository groupActivityRepository;
+
+    public GroupActivityService(GroupActivityRepository groupActivityRepository) {
+        this.groupActivityRepository = groupActivityRepository;
+    }
 }
